0c9027ab279df4ee2d6a4bac8e23027526494572
Anton Malko <anton.malko@anu.edu.au>
2025-06-20 14:23:23 +1000

    Initial commit

---
+++ install.R
@@ -0,0 +1,16 @@
+install.packages(c("rmarkdown",
+                   "ggplot2",
+                   "lme4",
+                   "here",
+                   "tidyr",
+                   "dplyr",
+                   "effectsize",
+                   "visreg",
+                   "car",
+                   "lmerTest",
+                   "HLMdiag",
+                   "tinytex"))
+
+install.packages("https://benjamins.com/sites/z.195/download/r_package/Rling_1.0.tar.gz")
+
+tinytex::install_tinytex()
